159c86a6c11c39aaa91abf18b86cda96dab10301
CJ Apel <cjapel@CJs-MacBook-Air.local>
2015-11-20 20:54:40 -0800

    can click on Download Snippet buttons and grap reference to snippet title. Saves and populates new snippets

--- client/chrome-ext/popup.js
+++ client/chrome-ext/popup.js
@@ -4,23 +4,69 @@
 
 
 document.addEventListener('DOMContentLoaded', function(){
-var log = function (stuff) {
-  var text = $('#content').html();
-  $('#content').html(text+'\n'+stuff);
-};
 
-  $(".add").click(function(event) {
+  var log = function (stuff) {
+    var li = document.createElement('li');
+    var p = document.createElement('p');
+    var dl = document.createElement('button');
+    dl.innerText = "Download Snippet";
+    dl.addEventListener('click', function (e) {
+      e.preventDefault();
+      console.log(e.target.parentNode.childNodes[1].innerText);
+    });
+    p.innerText = stuff;
+    li.appendChild(dl);
+    li.appendChild(p);
+    content.appendChild(li);
+  };
+
+  var populateDiv = function (source, div) {
+    source.forEach(function (obj) {
+      var title = Object.keys(obj)[0];
+      log(title);
+    });
+  }
+  // initialize new snippet store array if it doesn't already exist in local storage
+  chrome.storage.local.get('snippetStore', function (snippetStore) {
+    if (!Array.isArray(snippetStore.snippetStore)) {
+      chrome.storage.local.set({ 'snippetStore': [] }, function () {
+        console.log('created new snippet store');
+      })
+    }
+    else {
+      populateDiv(snippetStore.snippetStore, content);
+    }
+  });
+
+
+  // cache DOM calls
+  var content = document.getElementById('content');
+  var add = document.getElementById('add');
+  var title = document.getElementById('title');
+  var scope = document.getElementById('scope');
+  var prefix = document.getElementById('prefix');
+  var snippet = document.getElementById('snippet');
+
+
+  add.addEventListener('click', function(event) {
     event.preventDefault();
+    var container = {};
     var snippet = {};
-    snippet.title = $("#title").val();
-    snippet.scope = $("#scope").val();
-    snippet.prefix = $("#prefix").val();
-    snippet.text = $("#snippet").val();
-    var snippetJSON = JSON.stringify(snippet);
-    log(snippetJSON);
-    // chrome.storage.sync.set({'first': snippetJSON}, function () {
-    //   log('success');
-    // });
+
+    snippet.title = title.value;
+    snippet.scope = scope.value;
+    snippet.prefix = prefix.value;
+    snippet.text = snippet.value;
+
+    container[snippet.title] = JSON.stringify(snippet);
+
+    chrome.storage.local.get('snippetStore', function (snippetStore) {         // grab snippetStore array
+      snippetStore.snippetStore.push(container);                              // push the new object :: {title: objJSONString}
+      chrome.storage.local.set({'snippetStore': snippetStore.snippetStore});   // set snippetStore as new updated array
+      
+      content.innerHTML = '';                                                 // clear list before repopulating
+      populateDiv(snippetStore.snippetStore, content);
+    });
     });
   });
 
