76a96c9a38f26716df4fa1393ec5c6fa7964c2cd
Kim Ha <kimberlygha@gmail.com>
2015-11-30 11:22:12 -0800

    ran grunt doccify

--- client/js/addSnippet.js
+++ client/js/addSnippet.js
@@ -9,7 +9,9 @@ angular.module('sniphub.addSnippet', [])
   }
   //call another method in services
   $scope.postSnippet = function (  user, text, title, tabPrefix, scope, tag ) {
+    // regex to remove spaces between tags and commas
     var tag = tag.replace(/,\s+/g, ',').replace(/\s+,/g, '');
+    // send the tag information to sniphub services 
     SniphubServices.addSnippet( user, text, title, tabPrefix, scope, tag ).then(function ( response ) {
       $scope.submitted();
     });
